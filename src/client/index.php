760c2a16635ecd65d670c47af0a183f9fe7aa700
Juan Ruvalcaba <juan.ruvalcaba@beliveo.com>
2017-12-01 17:28:04 -0600

    Add index file to bbox

--- src/client/index.php
+++ src/client/index.php
@@ -1,4 +1,21 @@
-<?php ?>
-<div>
- Hello bBox! This is the new index file
-</div>
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>bBox login</title>
+		<link href="src/client/main.css" rel="stylesheet" type="text/css"/>
+	</head>
+	<body>
+			<div class="contenedor-form">
+				<form action="#" class="form">
+					<h2>Iniciar Sesión</h2>
+					<input type="text" placeholder="Usuario" required>
+					<input type="password" placeholder="Contraseña" required>
+					<input type="submit" value="Iniciar Sesión">
+				</form>
+				<div class="reset-password">
+						<a href="#">Olvidé mi contraseña</a>
+				</div>
+			</div>
+	</body>
+</html>
