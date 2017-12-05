28994efb502405675c726ec022fc554cf11aa033
Juan Ruvalcaba <juan.ruvalcaba@beliveo.com>
2017-12-04 19:43:08 -0600

    Change css file name

--- src/client/index.php
+++ src/client/index.php
@@ -3,19 +3,15 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>Beliveo bBox - Powered by people</title>
-		<link href="src/client/main.css" rel="stylesheet" type="text/css"/>
+		<link href="src/client/css/styles.css" rel="stylesheet" type="text/css"/>
 	</head>
 	<body>
 			<div class="contenedor-form">
 				<form action="#" class="form">
-<<<<<<< HEAD
 					<div class="logo-div">
-						<h2>Iniciar Sesión</h2>
+						<h2>Login</h2>
 						<img src="src/client/img/beliveo logo.png" border="0">
 					</div>
-=======
-					<h2>Login</h2>
->>>>>>> 99527f648c56a572867bc7e1f5c359d8d43a8ded
 					<input type="text" placeholder="Usuario" required>
 					<input type="password" placeholder="Contraseña" required>
 					<input type="submit" value="Iniciar Sesión">
