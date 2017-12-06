53576ec77949f4cc9ec73133250a007ef3383466
Salvador Gonzalez <salvador.gonzalez@beliveo.com>
2017-12-06 09:49:19 -0600

    Añadir codigo de slim a index.php

--- src/client/index.php
+++ src/client/index.php
@@ -1,4 +1,15 @@
-<!doctype html>
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require '../vendor/autoload.php';
+
+$app = new \Slim\App;
+$app->get('/', function () {
+    echo "hola";
+});
+$app->run();
+?>
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
