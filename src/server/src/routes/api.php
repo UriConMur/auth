a0c24a1f7c40b6745aa93d5bf76d40ddbaa742f1
Uriel Contreras <uriel.contreras@beliveo.com>
2017-12-21 12:38:36 -0600

    Add Husky

--- src/server/src/routes/api.php
+++ src/server/src/routes/api.php
@@ -22,16 +22,15 @@ $app->post(
         $case = 2;
         $username = trim($request->getParam("user"));
         $pass = trim($request->getParam("password"));
-        
+
         $iterations = 100;
         $seed = "f%uuu%erw9875487ot56.2{dskj-*/-*";
         $enc_pass = hash_pbkdf2("sha256", $pass, $seed, $iterations, 35);
-        
         $data = join("|-|", array($username, $enc_pass));
         $editor = 100000;
         $sql = "CALL sp_login_user_get(:case, :data, :editor)";
         $results = getDBData($sql, $case, $data, $editor);
-        
+
         $body = new stdClass();
         $status = new stdClass();
         $user = new stdClass();
@@ -42,7 +41,7 @@ $app->post(
             $body->user = $user;
             $status->code = 200;
             $status->message = "User found";
-            
+
         } else {
             $status->code = 404;
             $status->message = "User not found";
