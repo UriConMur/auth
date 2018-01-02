44ab8e44a57e974def546c26cd799ce1513746d1
Alan Martinez 393 <alan.martinez.393@beliveo.corp>
2018-01-02 11:49:02 -0600

    test/codesniffer

--- src/client/pages/login/js/index.js
+++ src/client/pages/login/js/index.js
@@ -2,7 +2,7 @@ $(document).ready(
     function () {
         var api_url = "src/server/public";
 
-        var get_user =  function (e) {
+        var get_user =  function (e)
             e.preventDefault();
             var user = $("#username").val();
             var pass = $("#password").val();
