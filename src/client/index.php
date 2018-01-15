db3352559a6a3571ccbd827576b66fa5643e3438
Uriel Contreras <uriel.contreras@beliveo.com>
2018-01-15 17:52:50 -0600

    Fix Sniffer

--- src/client/index.php
+++ src/client/index.php
@@ -10,21 +10,21 @@
     <?php
       require '../../config/environment.php';
       $cookie_name = 'unou';
-      if (! isset($_COOKIE[ $cookie_name ])) {
-          include 'pages/login/index.html';
-      } else {
-          if (isset($_GET['p'])) {
-              $redirect = $_GET['p'];
-              include "pages/{$redirect}/index.html";
-          } else {
-              if (isset($_GET['redirect_to'])) {
-                  $redirect = $_GET['redirect_to'];
-                  header("location: $redirect");
-              } else {
-                  include 'pages/home/index.html';
-              }
-          }
-      }
+    if (! isset($_COOKIE[ $cookie_name ])) {
+        include 'pages/login/index.html';
+    } else {
+        if (isset($_GET['p'])) {
+            $redirect = $_GET['p'];
+            include "pages/{$redirect}/index.html";
+        } else {
+            if (isset($_GET['redirect_to'])) {
+                $redirect = $_GET['redirect_to'];
+                header("location: $redirect");
+            } else {
+                include 'pages/home/index.html';
+            }
+        }
+    }
     ?>
 </div>
 
