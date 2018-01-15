45086a387d9da7edbf210bb0bc1643c3c9c4cc4e
Uriel Contreras <uriel.contreras@beliveo.com>
2018-01-15 17:51:57 -0600

    Merge changes from App Template

--- src/client/index.php
+++ src/client/index.php
@@ -1,19 +1,33 @@
-<?php
-require '../../config/environment.php';
+<!DOCTYPE html>
+<html>
+<head>
+<title>Beliveo</title>
+  <link rel="stylesheet" type="text/css" href="src/client/styles/main.css">
+</head>
+<body>
+<?php require_once '../../node_modules/beliveo-components/components/header.php'; ?>
+<div class="body_container">
+    <?php
+      require '../../config/environment.php';
+      $cookie_name = 'unou';
+      if (! isset($_COOKIE[ $cookie_name ])) {
+          include 'pages/login/index.html';
+      } else {
+          if (isset($_GET['p'])) {
+              $redirect = $_GET['p'];
+              include "pages/{$redirect}/index.html";
+          } else {
+              if (isset($_GET['redirect_to'])) {
+                  $redirect = $_GET['redirect_to'];
+                  header("location: $redirect");
+              } else {
+                  include 'pages/home/index.html';
+              }
+          }
+      }
+    ?>
+</div>
 
-$cookie_name = 'unou';
-if (! isset($_COOKIE[ $cookie_name ])) {
-    include 'pages/login/index.html';
-} else {
-    if (isset($_GET['p'])) {
-        $redirect = $_GET['p'];
-        include "pages/{$redirect}/index.html";
-    } else {
-        if (isset($_GET['redirect_to'])) {
-            $redirect = $_GET['redirect_to'];
-            header("location: $redirect");
-        } else {
-            include 'pages/home/index.html';
-        }
-    }
-}
+<?php require_once '../../node_modules/beliveo-components/components/footer.php'; ?>
+</body>
+</html>
