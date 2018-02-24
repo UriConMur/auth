af231f7a73d112fd7e26f8c19ccd35fba2c78def
Uriel Contreras <uriel.contreras@beliveo.com>
2018-02-24 14:06:21 -0600

    Fix file config

--- config/environment.php
+++ config/environment.php
@@ -1,7 +1,7 @@
 <?php
 
 $_ENV['DOMAIN'] = 'http://localhost';
-$_ENV['NAME_APP'] = 'app-template';
+$_ENV['NAME_APP'] = 'auth';
 $_ENV['LOGIN_URL'] = 'http://localhost/auth';
 $_ENV['API'] = '/src/server/public';
 $_ENV['DB_HOSTNAME'] = 'localhost';
