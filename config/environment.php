166918294e60bd76fafbca3d607d64440cb65794
Uriel Contreras <uriel.contreras@beliveo.com>
2018-02-24 14:04:18 -0600

    Add config files

---
+++ config/environment.php
@@ -0,0 +1,12 @@
+<?php
+
+$_ENV['DOMAIN'] = 'http://localhost';
+$_ENV['NAME_APP'] = 'app-template';
+$_ENV['LOGIN_URL'] = 'http://localhost/auth';
+$_ENV['API'] = '/src/server/public';
+$_ENV['DB_HOSTNAME'] = 'localhost';
+$_ENV['DB_USERNAME'] = 'root';
+$_ENV['DB_USERPASS'] = '123456';
+$_ENV['DB_SCHEMA'] = '';
+
+?>
